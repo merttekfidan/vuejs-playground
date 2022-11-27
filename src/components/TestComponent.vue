434729d4d65d7f7e2c193129a14720583de75368
Merdjan Tekfidan <merttekfidan@gmail.com>
2022-11-28 00:22:25 +0100

    first commit

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,28 @@
+<script>
+    export default{
+        data(){
+            return{ name:'Mert'}
+        },
+        methods:{
+            doSmth(){
+                this.name='Aleksa'
+            }
+        },
+        beforeMount(){
+            console.log('Before Mounted')
+        },
+        mounted(){
+            console.log('Mounted')
+        },
+        beforeUpdate(){
+            console.log(`${this.name}`)
+        },
+        updated(){
+            console.log(`${this.name}`)
+        }
+    }
+</script>
+
+<template>
+    <h1 @click='doSmth'>{{name}}</h1>
+</template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default({
+    data(){
+        return{
+            todoArray:[],
+            currentTodo:''
+        }
+    },
+    methods:{
+        addTodo(currentTodo){
+            this.todoArray.push(currentTodo)
+            this.currentTodo=''
+        }
+    }
+})
+</script>
+
+<template>
+    <input @keyup.enter="addTodo(currentTodo)" v-model='currentTodo' type="text"/>
+    <ul>
+        <li style='color:white' v-for="(todo,i) in todoArray" v-bind:key='i'>{{todo}}</li>
+    </ul>
+</template>
