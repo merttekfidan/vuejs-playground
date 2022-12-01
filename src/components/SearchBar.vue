e12ae4f0c9f2334450ad0fe197dc13c42140c508
Merdjan Tekfidan <merttekfidan@gmail.com>
2022-12-01 22:06:40 +0100

    Implement trial components

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type='text' placeholder='Type Something...' value=''/>
+  <button>Search</button>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
